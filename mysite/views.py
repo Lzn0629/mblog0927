744708e869544170d82a3a5bf0fc2f66433931f3
Lzn0629 <prkwh0629@gmail.com>
2023-10-11 14:20:36 +0800

    1011

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import Post
 
 # Create your views here.
+def homepage(request): #定義函式
+    posts = Post.objects.all()
+    post_lists = list() #動態陣列
+    for counter, post in enumerate(posts): #enumerate 自動數有幾筆
+        post_lists.append(f'No.{counter}-{post} <br>')  #br 網頁的換行 #f格式化
+    return HttpResponse(post_lists)
