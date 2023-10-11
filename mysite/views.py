c5d853728b447272c9c47d58e350de23e17d1871
Lzn0629 <prkwh0629@gmail.com>
2023-10-11 15:30:38 +0800

    1011

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mysite.models import Post
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+'''
 def homepage(request): #定義函式
     posts = Post.objects.all()
     post_lists = list() #動態陣列
     for counter, post in enumerate(posts): #enumerate 自動數有幾筆
         post_lists.append(f'No.{counter}-{post} <br>')  #br 網頁的換行 #f格式化
     return HttpResponse(post_lists)
+'''
